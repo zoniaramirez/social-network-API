4a6f419e4fb78bb148a3a6c1978173881ef7148b
Zonia Ramirez <jslnramirez@gmail.com>
2024-09-06 17:47:08 -0400

    fixed spelling

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -96,7 +96,7 @@ module.exports = {
       if (!thought) {
         return res.status(404).json({ message: 'No thought found with that ID' });
       }
-      res.json({ message: 'ReactionS successfully deleted!' });
+      res.json({ message: 'Reaction successfully deleted!' });
     } catch (err) {
       res.status(500).json(err);
     }
